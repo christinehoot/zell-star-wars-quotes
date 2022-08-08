820342b97df8437e10b510f88eee616c1232734b
Christine Huotari <cmhuotari@gmail.com>
2022-08-08 10:32:38 -0500

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
 console.log('May Node be with you')
-const express = require('express');
-const app = express();
 
-app.listen(3000, function() {
-    console.log('listening on 3000')
+const express = require('express');
+const bodyParser= require('body-parser')
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+const connectionString ='mongodb+srv://cmhoot:KYZdU7BGMLyPAdYE@cluster0.ymjgmet.mongodb.net/?retryWrites=true&w=majority'
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+    .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+    app.set('view engine', 'ejs')
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.get('/', (req, res) => {
+        quotesCollection.find().toArray()
+            .then(results => {
+                console.log(results)
+            })
+            .catch(error => console.error(error))
+        res.render('index.ejs', {})
+    })
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/')
+            })
+            .catch(error => console.error(error))
+    })
+    app.listen(3000, function() {
+        console.log('listening on 3000')
+    })
 })
+.catch(error => console.error(error))
+
+
+
+
 
